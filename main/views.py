a7ca097871645408803e1cd614dbfa928bd3a52d
MercurySon47 <4755e@mail.ru>
2024-09-01 17:47:15 +0700

    Добавили категории товаров в базу данных, промежуточный коммит с работы 01_09_24

--- main/views.py
+++ main/views.py
@@ -1,11 +1,15 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from django.shortcuts import render
+from goods.models import Categories
 
 
 def index(request):
+
+    categories = Categories.objects.all()
     context = {
         "title": "Home - Главная",
         "content": "Магазин мебели HOME",
+        "categories": categories,
     }
 
     return render(request, "main/index.html", context)
@@ -15,7 +19,7 @@ def about(request):
     context = {
         "title": "Home - О нас",
         "content": "О нас",
-        "text_on_page" : 'Мы прекрасный магазин мебели, у нас качественный товар'
+        "text_on_page": "Мы прекрасный магазин мебели, у нас качественный товар",
     }
 
     return render(request, "main/about.html", context)
