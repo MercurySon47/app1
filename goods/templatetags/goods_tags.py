4777994cc979d1e21a79036c2d88a328748b7e0c
MercurySon47 <4755e@mail.ru>
2024-09-05 15:47:17 +0700

    The product filter on the site page and urlencode , GET запрос в готовые ссылки с помощью шаблонного тега , для фильтров и пагинации сайта

--- goods/templatetags/goods_tags.py
+++ goods/templatetags/goods_tags.py
@@ -1,10 +1,21 @@
 
+# from urllib.parse import urlencode
+
 from django import template
+from django.utils.http import urlencode
 
 from goods.models import Categories
 
-register = template.Library()
+register = template.Library() #Создаем регестратор Шаблонных тегов
 
-@register.simple_tag()
+# Декоратор Регестрируем Шаблонный тег.
+@register.simple_tag() 
 def tag_categories():
-    return Categories.objects.all()
+    return Categories.objects.all() # Получаем все значения из таблицы базы данных
+
+# Декоратор Регестрируем Шаблонный тег с возможностью получения всех переменных из Контроллера Views.py
+@register.simple_tag(takes_context=True) 
+def change_params(context, **kwargs): # параметр **kwargs - получает не только значение но и название переменной. 
+    query = context['request'].GET.dict() # из переменной context получаем значение по ключу request получаем данные в виде словоря.
+    query.update(kwargs)
+    return urlencode(query) # Формируем из словоря готовую ссылку
--- goods/views.py
+++ goods/views.py
@@ -1,5 +1,5 @@
 # Контроллер отображения Страниц html
-from unicodedata import category
+# from unicodedata import category
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, get_list_or_404, render
 
@@ -11,11 +11,18 @@ from goods.models import Products
 def catalog(request, category_slug):
 
     page = request.GET.get('page', 1)
+    on_sale = request.GET.get('on_sale', None)
+    order_by = request.GET.get('order_by', None)
 
     if category_slug == 'all':
         goods = Products.objects.all()
     else:
         goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
+    
+    if on_sale:
+        goods = goods.filter(discount__gt=0)
+    if order_by and order_by!='default':
+        goods = goods.order_by(order_by)
 
     paginator =  Paginator(goods, 3)
     current_page = paginator.page(int(page))
