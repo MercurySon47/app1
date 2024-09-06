a316420c25c4fd76526479097aee5ec4a27734f3
MercurySon47 <4755e@mail.ru>
2024-09-06 15:58:20 +0700

    Full text search ! , Умный Текстовый поиск с выделием слов готов

--- goods/utils.py
+++ goods/utils.py
@@ -1,6 +1,11 @@
 # from re import search # Пример простого поиска
 # from django.db.models import Q # Пример простого поиска
-from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.contrib.postgres.search import (
+    SearchVector,
+    SearchQuery,
+    SearchRank,
+    SearchHeadline
+)
 from goods.models import Products
 
 
@@ -14,7 +19,30 @@ def q_search(query):
     vector = SearchVector("name", "description")
     query = SearchQuery(query)
 
-    return Products.objects.annotate(rank=SearchRank(vector, query)).order_by("-rank")
+    result = (
+        Products.objects.annotate(rank=SearchRank(vector, query))
+        .filter(rank__gt=0)
+        .order_by("-rank")
+    )
+    result = result.annotate(
+        headline=SearchHeadline(
+            "name",
+            query,
+            start_sel='<span style="background-color: yellow;">',
+            stop_sel="</span>",
+        )
+    )
+
+    result = result.annotate(
+        bodyline=SearchHeadline(
+            "description",
+            query,
+            start_sel='<span style="background-color: yellow;">',
+            stop_sel="</span>",
+        )
+    )
+
+    return result
 
     # Пример простого поиска
     # return Products.objects.filter(description__search=query)
