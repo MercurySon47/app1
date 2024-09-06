9ffad4f81b52950ae0f0ccbef711d94696673b26
MercurySon47 <4755e@mail.ru>
2024-09-06 10:47:03 +0700

    Добавлен поиск по ID, Словам в описании и Названию, сделан дамп BD через Fixtures

--- goods/utils.py
+++ goods/utils.py
@@ -6,3 +6,14 @@ from goods.models import Products
 def q_search (query): 
     if query.isdigit() and len(query) <=5: # Если посикровый запрос int ищем по id сотоящий до 5 знаков
         return Products.objects.filter(id=int(query)) # возвращаем Товар с выбранным id  
+    
+    keywords = [word for word in query.split() if len(word) > 2]
+    
+    q_objects = Q() # создаем переменную назначаем ей тип Q 
+
+    for token in keywords:
+        q_objects |= Q(description__icontains=token) # поиск по описанию добовляем Q обьект в список
+        q_objects |= Q(name__icontains=token) # поиск по названию добовляем Q обьект в список
+    
+    return Products.objects.filter(q_objects)
+
